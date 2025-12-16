f15333fdc3e2abf78dc651cd071006d8bdca1e78
erijosanchez <erijito.2@gmail.com>
2025-12-16 12:22:38 -0500

    Migrando sistema de encuestas 1.1

--- app/Http/Controllers/MarketingController.php
+++ app/Http/Controllers/MarketingController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class DashboardController extends Controller
+class MarketingController extends Controller
 {
     /**
      * Dashboard principal
